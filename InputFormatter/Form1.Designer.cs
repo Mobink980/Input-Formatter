93f9e5016f3af1aca2e3d4c78073125c23c2e100
Ali Karazmoodeh <mobink980@gmail.com>
2019-12-06 01:43:02 +0330

    Add files via upload

---
+++ InputFormatter/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace InputFormatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.labelFormat = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(132, 55);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(595, 22);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "\r\n";
+            this.textBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // label
+            // 
+            this.label.Location = new System.Drawing.Point(128, 29);
+            this.label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(354, 22);
+            this.label.TabIndex = 1;
+            this.label.Text = "Please Enter The Text: ";
+            // 
+            // labelFormat
+            // 
+            this.labelFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFormat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelFormat.Location = new System.Drawing.Point(313, 114);
+            this.labelFormat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFormat.Name = "labelFormat";
+            this.labelFormat.Size = new System.Drawing.Size(229, 29);
+            this.labelFormat.TabIndex = 2;
+            this.labelFormat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FORMAT:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(864, 332);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelFormat);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Input Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelFormat;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ InputFormatter/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InputFormatter
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        //The default Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        /*
+         * pattern is the format that we are getting input from the user:
+         s ==> character of a string including lowercase and uppercase
+         d ==> digit {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+         Other signs in the pattern have the same meaning.
+         */
+        readonly string pattern = "dd:ss:dd";
+        
+        //TextChanged function will be called, anytime the text inside the textBox is changed.
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Determining the label.
+            if (this.textBox.Text.Length < pattern.Length) 
+                label.Text = "Please Enter The Text: Entered " + this.textBox.Text.Length.ToString() + " out of " + pattern.Length.ToString();
+            else
+                label.Text = "You're All Done. Thank you!";
+            //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            labelFormat.Text = pattern; //Attaching pattern to the label labelFormat
+            //focus the user input text
+            FocusInputText();
+
+            if (this.textBox.Text.Length <= pattern.Length) //Update the textBox by correct input
+            {           
+                this.textBox.Text = InputFormatter(pattern);
+            }
+            else
+            {           //if the text overseeds the pattern, then remove extra text from the textBox    
+                this.textBox.Text = this.textBox.Text.Remove(pattern.Length , this.textBox.Text.Length - pattern.Length); //Remove the unvalid character
+            }
+           
+        }
+
+
+        #region Private Helpers
+        /// <summary>
+        /// This Method Helps to Maintain a Specific Pattern Within The textBox. 
+        /// </summary>
+        /// <param name="pattern">The pattern defined when calling the method</param>
+        private string InputFormatter(string pattern)
+        {
+
+            //focus the user input text
+            FocusInputText();
+
+            for (int i = 0; i < this.textBox.Text.Length; i++)
+            {
+                if ((int)this.textBox.Text[i] > 31) //Assuring that we got a character or the right ascci code
+                {
+                    if ((int)this.textBox.Text[i] > 47 && (int)this.textBox.Text[i] < 58) //If we got a digit
+                    {
+                        if (pattern[i] != 'd')
+                        { //If the pattern is not correct    
+                            return DeleteCharacter(i); //Remove the unvalid character
+                            
+                             //MessageBox.Show("Hi");
+
+                        }
+                    }
+                    
+                    else if ((int)this.textBox.Text[i] > 64 && (int)this.textBox.Text[i] < 91) //If we got a capital letter
+                    {
+                        if (pattern[i] != 's')
+                        { //If the pattern is not correct  
+                            return DeleteCharacter(i); //Remove the unvalid character
+                           // MessageBox.Show("Hi");
+                            
+                        }
+                    }
+
+                    else if ((int)this.textBox.Text[i] > 96 && (int)this.textBox.Text[i] < 123) //If we got a lowercase letter
+                    {
+                        if (pattern[i] != 's')
+                        { //If the pattern is not correct   
+                            return DeleteCharacter(i); //Remove the unvalid character                                                   
+                        }
+                    }
+
+                    else //If we got one of the allowed signs
+                    {
+                        if (pattern[i] != this.textBox.Text[i])
+                        { //If signs do not match
+                            return DeleteCharacter(i); //Remove the unvalid character
+                           // MessageBox.Show("Hi");
+                            
+                        }
+                    }
+                  
+                }
+           
+            }
+
+            //focus the user input text
+            FocusInputText();
+            return this.textBox.Text;
+        }
+
+        /// <summary>
+        /// Helps to remove an unvalid character from the textBox
+        /// </summary>
+        private string DeleteCharacter(int i)
+        {                      
+            // Delete the character to the left of the selection
+            string str = this.textBox.Text.Remove(i, 1);
+
+            //focus the user input text
+            FocusInputText();
+       
+            return str;
+        }
+
+        /// <summary>
+        /// Focuses the user's input text.
+        /// </summary>
+        private void FocusInputText()
+        {
+            this.textBox.Focus();
+
+            // Restore the selection start
+            textBox.SelectionStart = this.textBox.Text.Length;
+
+            // Set selection length to zero
+            textBox.SelectionLength = 0;
+        }
+        #endregion
+       
+    }
+}
